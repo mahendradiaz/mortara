8ee3b9e3170c44e1c77537a4d89380ddfffedc95
mahendradiaz <diazmahendra29.dm@gmail.com>
2022-09-23 19:25:38 +0700

    tambahan edit navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import './App.css';
 import 'remixicon/fonts/remixicon.css'
-import Home from './components/Home'
-import Navbar from './components/Navbar'
 import 'bulma/css/bulma.min.css';
 import Router from './router';
+// import 'semantic-ui-css/semantic.min.css'
 // import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 // import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,29 +4,31 @@ import Navbar from 'react-bootstrap/Navbar';
 
 function Menu() {
   return (
-    <Navbar collapseOnSelect expand="xxl" bg="light" variant="light">
-      <Container fluid className="outerNav">
-        <Navbar.Brand className="Logo" href="/">Mortara</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#features" hidden>Features</Nav.Link>
-            <Nav.Link href="#pricing" hidden>Pricing</Nav.Link>
-          </Nav>
-          <Nav>
-            <Nav.Link className="navigasi" eventKey={1} href="/">Home</Nav.Link>
-            <Nav.Link className="navigasi" eventKey={2} href="/AboutUs">
-              About Us
-            </Nav.Link>
-            <Nav.Link className="navigasi" eventKey={3} href="/Portfolio">Portfolio</Nav.Link>
-            <Nav.Link className="navigasi" eventKey={4} href="/Artikel">
-              Artikel
-            </Nav.Link>
-            <Nav.Link className="navigasi" eventKey={5} href="/Contact">Contact</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div className="outerNav">
+      <Navbar collapseOnSelect expand="xxl" bg="light" variant="light">
+        <Container fluid >
+          <Navbar.Brand className="Logo" href="/">Mortara</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#features" hidden>Features</Nav.Link>
+              <Nav.Link href="#pricing" hidden>Pricing</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link className="navigasi" href="/">Home</Nav.Link>
+              <Nav.Link className="navigasi" href="/AboutUs">
+                About Us
+              </Nav.Link>
+              <Nav.Link className="navigasi" href="/Portfolio">Portfolio</Nav.Link>
+              <Nav.Link className="navigasi" href="/Artikel">
+                Artikel
+              </Nav.Link>
+              <Nav.Link className="navigasi" href="/Contact">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
   );
 }
 
