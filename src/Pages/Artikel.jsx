9ddeb80b2d25b0de0f48125a50e8fce3e4065017
mahendradiaz <diazmahendra29.dm@gmail.com>
2022-09-29 17:23:27 +0700

    mau embed ig

--- src/Pages/Artikel.jsx
+++ src/Pages/Artikel.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import Menu from '../components/Navbar'
+import { Container } from 'react-bootstrap'
+import '../style/home.css'
+import '../style/artikel.css'
 
 const Artikel = () => {
   return (
-    <Menu />
+    <>
+      <Menu />
+      <div className="containerArtikel">
+        <div className="artikel">
+          <div className="textArtikel">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur nesciunt assumenda totam commodi nostrum ducimus illum, perspiciatis voluptatum aperiam quibusdam.</p>
+          </div>
+          <div className="embedInstagram">
+          
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
