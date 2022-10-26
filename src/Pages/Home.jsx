d127fdc09dbe39cc5e161d39b7e7ab8ca3c3f250
mahendradiaz <diazmahendra29.dm@gmail.com>
2022-10-26 15:16:03 +0700

    light mode only

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -31,25 +31,25 @@ const Home = () => {
         </div>
     </Container>
 
-<Container fluid className="content2">
-    <div className="whyMortara">
-        <div className="container-header">
-            <img src={ MortaraLogo } alt="" className="logoMortara"/>
-        </div>
-        <div className="container-body">
-            <div className="textMortara">
-                <h1>Why Mortara?</h1>
-                <p className="mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, ipsam officia itaque debitis doloremque ipsum earum natus cumque perspiciatis voluptatibus. Dignissimos delectus suscipit corporis ad non vitae numquam cumque sed accusamus? Ipsam delectus, esse quasi impedit voluptate dolorum reiciendis quis facilis nemo eum accusantium adipisci error exercitationem laborum laudantium. Nulla.</p>
+    <Container fluid className="content2">
+        <div className="whyMortara">
+            <div className="container-header">
+                <img src={ MortaraLogo } alt="" className="logoMortara"/>
+            </div>
+            <div className="container-body">
+                <div className="textMortara">
+                    <h1>Why Mortara?</h1>
+                    <p className="mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, ipsam officia itaque debitis doloremque ipsum earum natus cumque perspiciatis voluptatibus. Dignissimos delectus suscipit corporis ad non vitae numquam cumque sed accusamus? Ipsam delectus, esse quasi impedit voluptate dolorum reiciendis quis facilis nemo eum accusantium adipisci error exercitationem laborum laudantium. Nulla.</p>
+                </div>
+            </div>
+            <div className="image-container">
+                <div className="imgMortara">
+                    <img src={ whyMortara } alt="" className="img-mortara"/>
+                    <h3 className="mt-5 p-3">#nothingbutperfection</h3>
+                </div>
             </div>
         </div>
-        <div className="image-container">
-            <div className="imgMortara">
-                <img src={ whyMortara } alt="" className="img-mortara"/>
-                <h3 className="mt-5 p-3">#nothingbutperfection</h3>
-            </div>
-        </div>
-    </div>
-</Container>
+    </Container>
 
     <div className="hero min-h-screen">
         <div className="content-hero">
