dac58a9156c3c0074c02b4dee5fbfabbfe573146
mahendradiaz <diazmahendra29.dm@gmail.com>
2022-10-26 16:09:36 +0700

    fix close button banget

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,24 +4,32 @@ import Navbar from 'react-bootstrap/Navbar';
 import LogoMortara from '../img/logo-mortara.png'
 import { Offcanvas } from 'react-bootstrap';
 import CloseButton from 'react-bootstrap/CloseButton';
+import { useState } from 'react';
 
 function Menu() {
+
+  const [show, setShow] = useState(false);
+  
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true)
   return (
       <>
       {["xxl"].map((expand) => (
         <Navbar expand={expand} bg="light" className="Navbar mb-3" fixed="top">
           <Container fluid>
             <Navbar.Brand href="/" className="Logo"><img src={LogoMortara} alt="" /></Navbar.Brand>
-            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`}/>
-            <Navbar.Offcanvas
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} onClick={handleShow}/>
+            <Navbar.Offcanvas show={show} onHide={handleClose}
               id={`offcanvasNavbar-expand-${expand}`}
               aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
               placement="end"
             >
-              <Offcanvas.Header closeButton={true}>
+              <Offcanvas.Header>
                 <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`} className="Logo">
                   <img src={LogoMortara} alt="" />
                 </Offcanvas.Title>
+                <CloseButton onClick={handleClose} />
               </Offcanvas.Header>
               <Offcanvas.Body>
                 <Nav className="justify-content-end flex-grow-1 pe-3">
